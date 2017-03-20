a63db492052dc70c1e2bd2b2b771d3277b59f711
Albert <hvgmbdmg@gmail.com>
2017-03-21 01:07:44 +0800

    Hello World (Not Finish)
    Color Scheme, some package, sublime, chrome, etc.

---
+++ Hello-World.js
@@ -0,0 +1,34 @@
+function sayHi() {
+    alert("Hello World!");
+}
+
+//ar sayhi = sayHi();
+//sayhi(); 
+
+/*
+< !DOCTYPE html >
+    < html >
+    < head >
+    < title > I am title. < /title> < script >
+
+function sayHi() {
+    debug.Log("Hello World!");
+}
+
+< /script>
+
+
+< style >
+#simple {
+	font: bold 15pt sans;
+	padding: 8px;
+}
+
+
+
+    < /style> < /head > < body >
+
+    sayHi();
+
+< /body> < /html >
+*/
