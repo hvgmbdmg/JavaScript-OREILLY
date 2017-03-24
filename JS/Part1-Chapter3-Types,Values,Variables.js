5c8280ee1a17446c9099ae6357a655e23d60f251
Albert <hvgmbdmg@gmail.com>
2017-03-24 21:00:55 +0800

    Revert "Ch3.5 Part Finish"
    This reverts commit 6e50270e154a97e5f84cb6acb242594a24a941ab.

--- JS/Part1-Chapter3-Types,Values,Variables.js
+++ JS/Part1-Chapter3-Types,Values,Variables.js
@@ -33,46 +33,5 @@ function dateTest() {
 function escapeSeqTest(){
     document.write("First Line\nSecond Line\nThird Line\n");
     document.write("\tTab and other test: \t\"\t\'\n");
+
 }
-
-function stringTest(){
-    var s = "hello, world";      // Start with some text.
-    document.write("\n\nString s is: " + s + "\n");
-
-    // => "h": the first character.
-    document.write("First character is h: " + s.charAt(0) + "<br>");
-    // => "d": the last character.
-    document.write("Last character is d: " + s.charAt(s.length-1) + "<br>");
-    // => "ell": the 2nd, 3rd and 4th characters.
-    document.write("1~4 character is d: " + s.substring(1,4) + " or "+ s.slice(1,4) + "<br>");
-    // => "rld": last 3 characters
-    document.write("last 3 character is d: " + s.slice(-3) + "<br>");
-    
-    // => 2: position of first letter l.
-    document.write("Where is the first l: " + s.indexOf("l") + "<br>");
-    // => 10: position of last letter l.
-    document.write("Where is the last 'l': " + s.lastIndexOf("l") + "<br>");
-    // => 3: position of first "l" at or after 3
-    document.write("position of first 'l at or after 3: " + s.indexOf("l", 3) + "\n\n");
-
-    var str = "original equipment manufacturer";
-    var s2 = "equip is at position " + str.indexOf("equip");
-    s2 += "<br />";
-    s2 += "abc is at position " + str.indexOf("abc");
-    document.write(s2 + '\n');
-
-    // => ["hello", "world"] split into substrings
-    document.write("Split string: " + s.split(", ") + "<br>");
-    // => "Hello, world": replaces all instances
-    document.write("'H' replaces 'h' all instances: " + s.replace("h", "H") + "<br>");
-    // => "HELLO, WORLD"
-    document.write("All upper characters: " + s.toUpperCase() + "<br>");
-}
-
-function patternMatchTest(){
-    var text = "R65Eew3431fe53fds";      // Start with some text.
-    var pattern = /[1-9][0-9]*/
-    document.write("\nString is: " + text + "\n");
-    document.write("New string: " + text.match(pattern) + "<br>");
-}
-
